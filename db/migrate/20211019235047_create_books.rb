8e5f0d55d35b69a06f859a3898bbf1af96a60e7b
Mehmet <mehmetk6158@gmail.com>
2021-10-19 20:17:34 -0400

    migrate with creating a different column

---
+++ db/migrate/20211019235047_create_books.rb
@@ -0,0 +1,13 @@
+class CreateBooks < ActiveRecord::Migration[6.1]
+  def change
+    create_table :books do |t|
+      
+      t.string :title
+      t.string :description
+      t.string :isbn
+      t.string :summary
+
+      t.timestamps
+    end
+  end
+end
